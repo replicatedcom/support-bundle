348b72e14ab7edea9ce44e2b4e8660fc3854add6
Andrew Lavery <andrewl@replicated.com>
2017-10-02 10:19:54 -0700

    Removed skipped commands from the system bundle file These commands now referred to more directly

--- bundle/system.go
+++ bundle/system.go
@@ -1,84 +1,14 @@
 package bundle
 
 import (
-	"strings"
 	"sync"
 	"time"
 
 	"github.com/replicatedcom/support-bundle/metrics"
-	"github.com/replicatedcom/support-bundle/systemutil"
 	"github.com/replicatedcom/support-bundle/types"
 )
 
 func systemLogFiles(dataCh chan types.Data, completeCh chan bool, resultsCh chan types.Result, timeout time.Duration, args []string) error {
-	completeCh <- true
-	return nil
-}
-
-func systemFiles(dataCh chan types.Data, completeCh chan bool, resultsCh chan types.Result, timeout time.Duration, files []string) error {
-	var wg sync.WaitGroup
-
-	itemCompleteCh := make(chan bool)
-
-	var tasks = []Task{}
-	for _, file := range files {
-		tasks = append(tasks, Task{
-			Description: "Read file at " + file,
-			ExecFunc:    systemutil.ReadFile,
-			Timeout:     time.Duration(time.Second * 15),
-			Args:        []string{file},
-		})
-	}
-	wg.Add(len(tasks))
-
-	go func() {
-		for {
-			select {
-			case <-itemCompleteCh:
-				wg.Done()
-				break
-			}
-		}
-	}()
-
-	for _, task := range tasks {
-		_ = task.ExecFunc(dataCh, itemCompleteCh, resultsCh, task.Timeout, task.Args)
-	}
-
-	completeCh <- true
-	return nil
-}
-
-func systemCommands(dataCh chan types.Data, completeCh chan bool, resultsCh chan types.Result, timeout time.Duration, files []string) error {
-	var wg sync.WaitGroup
-
-	itemCompleteCh := make(chan bool)
-
-	var tasks = []Task{}
-	for _, file := range files {
-		tasks = append(tasks, Task{
-			Description: "Run command " + file,
-			ExecFunc:    systemutil.RunCommand,
-			Timeout:     time.Duration(time.Second * 15),
-			Args:        strings.SplitN(file, " ", 2),
-		})
-	}
-	wg.Add(len(tasks))
-
-	go func() {
-		for {
-			select {
-			case <-itemCompleteCh:
-				wg.Done()
-				break
-			}
-		}
-	}()
-
-	for _, task := range tasks {
-		_ = task.ExecFunc(dataCh, itemCompleteCh, resultsCh, task.Timeout, task.Args)
-	}
-
 	completeCh <- true
 	return nil
 }
