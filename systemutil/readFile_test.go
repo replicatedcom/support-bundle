177c1698d87a497c4e3ef1a64105b55bb0d92ea3
Andrew Lavery <andrewl@replicated.com>
2017-10-05 15:17:42 -0700

    Updated read file and run command tests to expect 1 returned data struct

--- systemutil/readFile_test.go
+++ systemutil/readFile_test.go
@@ -13,7 +13,7 @@ func TestReadFile(t *testing.T) {
 
 	datas, result, err := ReadFile(context.Background(), commandStrings)
 	require.NoError(t, err)
-	require.Equal(t, 3, len(datas), "Expected 3 data structs to be returned")
+	require.Equal(t, 1, len(datas), "Expected 1 data struct to be returned")
 	require.NoError(t, result.HumanError)
 	require.NoError(t, result.RawError)
 	require.NoError(t, result.JSONError)
--- systemutil/runCommand_test.go
+++ systemutil/runCommand_test.go
@@ -16,7 +16,7 @@ func TestRunCommand(t *testing.T) {
 
 	datas, result, err := RunCommand(context.Background(), commandStrings)
 	require.NoError(t, err)
-	require.Equal(t, 3, len(datas), "Expected 3 data structs to be returned")
+	require.Equal(t, 1, len(datas), "Expected 1 data struct to be returned")
 	require.NoError(t, result.HumanError)
 	require.NoError(t, result.RawError)
 	require.NoError(t, result.JSONError)
