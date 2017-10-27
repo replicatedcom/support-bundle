cf306d10ce0cc939a1889acd6b97442382867193
Dex <dexter.i.horthy@gmail.com>
2017-10-27 11:46:40 -0700

    starting on scrub test

---
+++ tests/ginkgo/scrub_test.go
@@ -0,0 +1,16 @@
+package ginkgo
+
+import (
+	"log"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Scrubbing secrets from file", func() {
+	BeforeEach(func() {
+
+	})
+	Specify("Then Replicated should redirect to the new hostname", func() {
+	})
+});
