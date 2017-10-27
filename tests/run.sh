5a17dcde1118f66c6a0b78e057cfca94cac5863b
Shailie Jain <jshailiegmail.com@Shailies-MacBook-Pro.local>
2017-10-27 12:41:52 -0700

    small tweak in run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -3,5 +3,6 @@
 docker run --rm \
     -v `pwd`:/go/src/github.com/replicatedcom/support-bundle \
     -w /go/src/github.com/replicatedcom/support-bundle/tests \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     golang:1.9-alpine \
     go test -v .
