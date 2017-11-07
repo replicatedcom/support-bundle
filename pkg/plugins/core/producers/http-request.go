20972dab6486adfc31942cd0ea9229be68987c01
Ethan Mosbaugh <ethan@replicated.com>
2017-11-07 13:46:35 -0800

    add tls

--- pkg/plugins/core/producers/http-request.go
+++ pkg/plugins/core/producers/http-request.go
@@ -3,16 +3,38 @@ package producers
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
 
+	"github.com/docker/go-connections/sockets"
 	"github.com/pkg/errors"
 	"github.com/replicatedcom/support-bundle/pkg/types"
 	jww "github.com/spf13/jwalterweatherman"
 	"golang.org/x/net/context/ctxhttp"
 )
+
+var insecureClient, secureClient *http.Client
+
+func init() {
+	trSecure := new(http.Transport)
+	sockets.ConfigureTransport(trSecure, "tcp", "")
+	// TODO: possibly use go-connections/tlsconfig
+	trSecure.TLSClientConfig = &tls.Config{InsecureSkipVerify: false}
+	secureClient = &http.Client{
+		Transport: trSecure,
+	}
+
+	trInsecure := new(http.Transport)
+	sockets.ConfigureTransport(trInsecure, "tcp", "")
+	// TODO: possibly use go-connections/tlsconfig
+	trInsecure.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	insecureClient = &http.Client{
+		Transport: trInsecure,
+	}
+}
 
 func HTTPRequest(opts types.HTTPRequestCommandOptions) types.StreamProducer {
 	return func(ctx context.Context) (io.Reader, error) {
@@ -27,7 +49,14 @@ func HTTPRequest(opts types.HTTPRequestCommandOptions) types.StreamProducer {
 			return nil, errors.Wrap(err, "new request")
 		}
 
-		resp, err := ctxhttp.Do(ctx, nil, req)
+		var client *http.Client
+		if opts.Insecure {
+			client = insecureClient
+		} else {
+			client = secureClient
+		}
+
+		resp, err := ctxhttp.Do(ctx, client, req)
 		if err != nil {
 			return nil, errors.Wrap(err, "make request")
 		}
