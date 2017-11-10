b04f125c5e5c9c23918a18609a25f139cbbb7013
divolgin <dmitriy@replicated.com>
2017-11-10 13:57:31 -0800

    [ch3351] read-file and read-host-file produce tar archives

--- pkg/plans/stream-source.go
+++ pkg/plans/stream-source.go
@@ -1,10 +1,13 @@
 package plans
 
 import (
+	"archive/tar"
 	"context"
 	"errors"
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/replicatedcom/support-bundle/pkg/types"
@@ -14,6 +17,8 @@ import (
 type StreamSource struct {
 	// Producer provides the seed data for this task as an io.Reader
 	Producer func(context.Context) (io.Reader, error)
+	// StreamFormat describe stream format returned by Producer.  Only "" and "tar" are supported.
+	StreamFormat string
 	// RawScrubber, if defined, rewrites the raw data to to remove sensitive data
 	RawScrubber func([]byte) []byte
 	// Template, if defined, renders structured data in a human-readable format
@@ -36,24 +41,23 @@ func (task *StreamSource) Exec(ctx context.Context, rootDir string) []*types.Res
 
 	results := []*types.Result{}
 
-	descriptionResults := []*types.Result{}
-	if raw {
-		descriptionResults = append(descriptionResults, &types.Result{Description: task.RawPath})
-	}
-	if jsonify {
-		descriptionResults = append(descriptionResults, &types.Result{Description: task.JSONPath})
-	}
-	if human {
-		descriptionResults = append(descriptionResults, &types.Result{Description: task.HumanPath})
-	}
-
 	if !(raw || jsonify || human) {
 		return results
 	}
 
+	if raw {
+		results = append(results, &types.Result{Description: task.RawPath})
+	}
+	if jsonify {
+		results = append(results, &types.Result{Description: task.JSONPath})
+	}
+	if human {
+		results = append(results, &types.Result{Description: task.HumanPath})
+	}
+
 	if task.Producer == nil {
 		err := errors.New("no data source defined for task")
-		return resultsWithErr(err, descriptionResults)
+		return resultsWithErr(err, results)
 	}
 
 	if task.Timeout != 0 {
@@ -64,51 +68,92 @@ func (task *StreamSource) Exec(ctx context.Context, rootDir string) []*types.Res
 
 	data, err := task.Producer(ctx)
 	if err != nil {
-		return resultsWithErr(err, descriptionResults)
+		return resultsWithErr(err, results)
 	}
 	if closer, ok := data.(io.Closer); ok {
 		defer closeLogErr(closer)
 	}
 
+	if task.StreamFormat == "" {
+		return task.execStream(ctx, rootDir, "", data)
+	}
+
+	if task.StreamFormat != "tar" {
+		err := fmt.Errorf("unsupported stream format: %q", task.StreamFormat)
+		return resultsWithErr(err, results)
+	}
+
+	tarReader := tar.NewReader(data)
+	for {
+		hdr, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return resultsWithErr(err, results)
+		}
+
+		if hdr.FileInfo().IsDir() {
+			continue
+		}
+
+		moreResults := task.execStream(ctx, rootDir, hdr.Name, tarReader)
+		results = append(results, moreResults...)
+	}
+
+	return results
+}
+
+func (task *StreamSource) execStream(ctx context.Context, rootDir string, filePath string, reader io.Reader) []*types.Result {
+	raw := task.RawPath != ""
+	jsonify := task.JSONPath != ""
+	human := task.HumanPath != ""
+
+	results := []*types.Result{}
+
 	if task.RawScrubber != nil {
 		scrubbedReader, scrubbedWriter := io.Pipe()
-		go filterStreams(data, scrubbedWriter, task.RawScrubber)
-		data = scrubbedReader
+		go filterStreams(reader, scrubbedWriter, task.RawScrubber)
+		reader = scrubbedReader
 	}
 
 	rawResult := types.Result{}
 	jsonResult := types.Result{}
 	humanResult := types.Result{}
 
+	rawPath := filepath.Join(task.RawPath, filePath)
+	jsonPath := filepath.Join(task.JSONPath, filePath)
+	humanPath := filepath.Join(task.HumanPath, filePath)
+
 	// first write to one file
 	if raw {
-		writeResult(ctx, rootDir, task.RawPath, &rawResult, data)
+		writeResult(ctx, rootDir, rawPath, &rawResult, reader)
 	} else if jsonify {
-		writeResult(ctx, rootDir, task.JSONPath, &jsonResult, data)
+		writeResult(ctx, rootDir, jsonPath, &jsonResult, reader)
 	} else if human {
-		writeResult(ctx, rootDir, task.HumanPath, &humanResult, data)
+		writeResult(ctx, rootDir, humanPath, &humanResult, reader)
 	}
 
 	// then link to any other requested paths
 	if raw && jsonify {
 		jsonResult = rawResult
 		if rawResult.Path != "" {
-			os.Link(task.RawPath, task.JSONPath)
-			jsonResult.Path = task.JSONPath
+			os.Link(rawPath, jsonPath)
+			jsonResult.Path = jsonPath
 		}
 	}
 	if raw && human {
 		humanResult = rawResult
 		if rawResult.Path != "" {
-			os.Link(task.RawPath, task.HumanPath)
-			humanResult.Path = task.HumanPath
+			os.Link(rawPath, humanPath)
+			humanResult.Path = humanPath
 		}
 	}
 	if jsonify && human {
 		humanResult = jsonResult
 		if jsonResult.Path != "" {
-			os.Link(task.JSONPath, task.HumanPath)
-			humanResult.Path = task.HumanPath
+			os.Link(jsonPath, humanPath)
+			humanResult.Path = humanPath
 		}
 	}
 
