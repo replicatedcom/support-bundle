75b6b2da95e2870c50133c8e7add7752438ef381
Gerred Dillon <hello@gerred.org>
2017-12-01 09:40:05 -0700

    use a url if it is provided

--- pkg/bundle/generate.go
+++ pkg/bundle/generate.go
@@ -3,7 +3,10 @@ package bundle
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"time"
@@ -16,6 +19,14 @@ import (
 
 // Generate a new support bundle and write the results as an archive at pathname
 func Generate(tasks []types.Task, timeout time.Duration, pathname string) error {
+	var isURL bool
+
+	url, err := url.Parse(pathname)
+	if err == nil && url.Scheme == "https" {
+		isURL = true
+		pathname = "/tmp/support-bundle.tar.gz"
+	}
+
 	collectDir, err := ioutil.TempDir(filepath.Dir(pathname), "")
 	if err != nil {
 		return errors.Wrap(err, "Creating a temporary directory to store results failed")
@@ -57,9 +68,25 @@ func Generate(tasks []types.Task, timeout time.Duration, pathname string) error 
 	comp := compressor.NewTgz()
 	comp.SetTarConfig(compressor.Tar{TruncateLongFiles: true})
 	// trailing slash keeps the parent directory from being included in archive
+
 	if err := comp.Compress(collectDir+"/", pathname); err != nil {
 		return errors.Wrap(err, "Compressing results directory failed")
 	}
 
-	return nil
+	if isURL {
+		file, err := os.Open(pathname)
+		defer file.Close()
+
+		if err != nil {
+			return errors.Wrap(err, "finding the file that was just compressed")
+		}
+
+		fmt.Printf("Posting to %s\n", url.String())
+
+		_, err = http.Post(url.String(), "application/tar+gzip", file)
+
+		fmt.Println(err)
+	}
+
+	return err
 }
