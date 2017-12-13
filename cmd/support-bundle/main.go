d6fe5b215f1c1d9bd374a6b891d96ff707d00d5a
Gerred Dillon <hello@gerred.org>
2017-12-13 10:45:26 -0700

    move package to main

--- cmd/support-bundle/main.go
+++ cmd/support-bundle/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"os"
 
-	"github.com/replicatedcom/support-bundle/cmd"
 	"github.com/replicatedcom/support-bundle/pkg/cli"
 )
 
 func main() {
-	c := cmd.NewSupportBundleCommand(cli.NewCli())
+	c := NewSupportBundleCommand(cli.NewCli())
 	if err := c.Execute(); err != nil {
 		os.Exit(1)
 	}
