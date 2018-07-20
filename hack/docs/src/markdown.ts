11dad29f08a6ea838e661054bf484a980f7e394a
Robert Oh <robert.is.oh@gmail.com>
2018-07-20 12:55:15 -0700

    Add step to generate docs for lifecycle steps (#175)

--- hack/docs/src/markdown.ts
+++ hack/docs/src/markdown.ts
@@ -1,67 +1,8 @@
 import * as fs from "fs";
 import * as yaml from "js-yaml";
+import { SHARED_DOC, SHARED_SPEC_TEMPLATE } from "./template";
 
-const SHARED_DOC = `---
-categories:
-- support-bundle-yaml-specs
-date: 2018-01-17T23:51:55Z
-description: Reference Documentation for defining your Support Bundle collection and contents
-index: docs
-title: Support Bundle YAML Specs
-weight: "1"
-gradient: "purpleToPink"
----
-
-## Support Bundle Collection Specs
-
-Support Bundle collection specs can be used to define and customize what kinds diagnostic
-information you want to collect to debug your application. All Support Bundle specs support the following shared parameters:
-
-### Required Parameters
-
-- \`output_dir\` - The directory in the bundle to store the collection results
-
-### Optional Parameters
-
-- \`timeout_seconds\` - An amount of time to allow a collection to run before abandoning it
-
-- \`description\` - A description of the file(s) being collected
-
-- \`scrub\` - A \`regex\` and \`replace\` specification for removing sensitive data from files in the bundle
-
-- \`meta\` - A \`name\` and \`labels\` that can be used to organize and identify support bundle elements in generated bundles
-
-- \`include_empty\` - Allows empty files to be included in output
-
-### Usage
-
-An example is shown below for the \`os.read-file\` collector.
-
-\`\`\`yaml
-specs:
-  - os.read-file:
-      # path on the host
-      filepath: /etc/goodtool.conf
-      # path in the bundle
-      output_dir: /files/etc/goodtool-conf
-      # a description that will be included in the bundle
-      description: The GoodTool application configuration file
-      # give up if we can't read the file in 10 seconds
-      timeout_seconds: 10
-      # scrub anything that might be sensitive
-      scrub:
-        regex: (db_password|api_secret_key) = (.*)
-        replace: $1 = REDACTED
-      # metadata that will be included in the bundle
-      meta:
-        name: goodtool_conf
-        labels:
-          area: "configuration"
-          type: "readfile"
-      # Includes file in output even if empty
-      include_empty: true
-\`\`\`
-`;
+type SCHEMA_TYPE = "support-bundle-yaml-lifecycle" | "support-bundle-yaml-specs";
 
 export const name = "markdown";
 export const describe = "Build markdown for examples in top-level elements";
@@ -138,23 +79,29 @@ function parseParameters(specTypes: any, specType) {
   return {required, optional};
 }
 
-function maybeRenderExamples(specTypes: any, specType) {
+function maybeRenderExamples(specTypes: any, specType: string, schemaType: SCHEMA_TYPE) {
+  const schemaTypeMap: { [K in SCHEMA_TYPE]: string} = {
+    "support-bundle-yaml-lifecycle": "lifecycle",
+    "support-bundle-yaml-specs": "specs",
+  };
+
   let doc = "";
   if (specTypes[specType].examples) {
     for (const example of specTypes[specType].examples) {
+      const translatedSchemaType = schemaTypeMap[schemaType];
       doc += `
 ${"```yaml"}
-${yaml.safeDump({specs: [{[specType]: example}]})}${"```"}
+${yaml.safeDump({[translatedSchemaType]: [{[specType]: example}]})}${"```"}
 `;
     }
   }
   return doc;
 }
 
-function writeHeader(specTypes: any, specType) {
+function writeHeader(specTypes: any, specType: string, schemaType: SCHEMA_TYPE) {
   return `---
 categories:
-- support-bundle-yaml-specs
+- ${schemaType}
 date: 2018-01-17T23:51:55Z
 description: ${specTypes[specType].description || ""}
 index: docs
@@ -170,38 +117,46 @@ ${specTypes[specType].description || ""}
 `;
 }
 
-export const handler = (argv) => {
-  const schema = JSON.parse(fs.readFileSync(argv.infile).toString());
+const renderEnd = (schemaType: SCHEMA_TYPE) => {
+  if (schemaType === "support-bundle-yaml-specs") {
+    return SHARED_SPEC_TEMPLATE;
+  }
+  return "";
+};
 
-  fs.writeFileSync(`${argv.output}/shared.md`, SHARED_DOC);
+const generateDoc: (outputLocation: string, specTypes: any, schemaType: SCHEMA_TYPE) => (specType: string) => void = (outputLocation, specTypes, schemaType) => (specType) => {
+  console.log(`PROPERTY ${specType}`);
+  if (schemaType === "support-bundle-yaml-specs" && specType.indexOf(".") === -1) {
+    console.log(`SKIPPING ${specType}`);
+    return;
+  }
+  const cleanProperty = specType.replace(/\./g, "-");
+
+  let doc = "";
+  doc += writeHeader(specTypes, specType, schemaType);
+  doc += maybeRenderExamples(specTypes, specType, schemaType);
+
+  const {required, optional} = parseParameters(specTypes, specType);
+  doc += maybeRenderParameters(required, `Required`);
+  doc += maybeRenderParameters(optional, `Optional`);
+  doc += maybeRenderOutputs(specTypes, specType);
+  doc += renderEnd(schemaType);
+
+  fs.writeFileSync(`${outputLocation}/${cleanProperty}.md`, doc);
+};
+
+export const handler = (argv) => {
+  const { infile, output } = argv;
+  const schema = JSON.parse(fs.readFileSync(infile).toString());
+
+  fs.writeFileSync(`${output}/shared.md`, SHARED_DOC);
 
   const specTypes = schema.properties.specs.items.properties;
-  for (const specType of Object.keys(specTypes)) {
-    console.log(`PROPERTY ${specType}`);
-    if (specType.indexOf(".") === -1) {
-      console.log(`SKIPPING ${specType}`);
-      continue;
-    }
-    const cleanProperty = specType.replace(/\./g, "-");
+  const lifecycleSpecTypes = schema.properties.lifecycle.items.properties;
 
-    let doc = "";
-    doc += writeHeader(specTypes, specType);
-    doc += maybeRenderExamples(specTypes, specType);
+  const generateDocSpecTypes = generateDoc(`${output}/specs`, specTypes, "support-bundle-yaml-specs");
+  const generateDocLifecycleSpecTypes = generateDoc(`${output}/lifecycle`, lifecycleSpecTypes, "support-bundle-yaml-lifecycle");
 
-    const {required, optional} = parseParameters(specTypes, specType);
-    doc += maybeRenderParameters(required, `Required`);
-    doc += maybeRenderParameters(optional, `Optional`);
-    doc += maybeRenderOutputs(specTypes, specType);
-
-    doc += `
-
-<br>
-{{< note title="Shared Parameters" >}}
-This spec also inherits all of the required and optional [Shared Parameters](/api/support-bundle-yaml-specs/shared/)
-{{< /note >}}
-
-    `;
-
-    fs.writeFileSync(`${argv.output}/${cleanProperty}.md`, doc);
-  }
+  Object.keys(specTypes).forEach(generateDocSpecTypes);
+  Object.keys(lifecycleSpecTypes).forEach(generateDocLifecycleSpecTypes);
 };
